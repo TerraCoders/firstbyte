04647c21a027e69256a6df3cec41d091556c67fc
TerraCoders Web Dev <services@terracoders.support>
2021-05-12 14:18:52 -0600

    error.sh comments

--- error.sh
+++ error.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+# A script to find verbose error logging in your Drupal settings.php file
+# and turn it on. You need to have it there to begin with, commented out.
+# having it commented out is good for when you hit the WSOD. With this script,
+# you can rewrite the line without the '//' commenting it out.
+
 
 #find line number for commented verbose error logging
 line=$(awk '/error_level/{print NR}' /pathto/settings.php)
