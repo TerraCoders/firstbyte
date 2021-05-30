4287a1e68c1d8d84c631fde5f4d54e92155d9939
David <services@terracoders.support>
2021-05-30 06:57:36 -0600

    'ls with octal mode output'

---
+++ lso.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Use stat to print octal and rwx permissions on files in a directory
+# if argument unset, show permissions for contents of current directory
+# if 'h' argument is passed, include hidden files
+# otherwise, pass argument to stat as filepath
+if [ -z ${1} ]; then
+  stat -c "%a %A %n" *
+elif [ "$1" = h ]; then
+  stat -c "%a %A %n" .* *
+else
+  stat -c "%a %A %n" "$1"
+fi
+
+
+#[ "$1" = h ] && stat -c "%a %A %n" .* * || [ -z ${1} ] && stat -c "%a %A %n" * || stat -c "%a %A %n" "$1"
