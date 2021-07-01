20e459c32ba3871b4bd43e3d1c49bb069776eb73
David <services@terracoders.support>
2021-06-30 22:21:32 -0600

    'Git Pull / Git Push'

---
+++ push.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+current="$(git branch | grep "*" | cut -b 3-)"
+
+echo "You are currently checked out on origin/$current" | lolcat
+
+if [ "$current" == master ]; then
+  echo "Push to origin/master (y)?"
+  read -i "y" -e yn1
+  [ "$yn1" == y ] && git push origin master || echo "Fat fingers?"
+else
+  echo "Push to origin/$current (y)?"
+  read -i "y" -e yn2
+  [ "$yn2" == y ] && git push origin $current || echo "Fat fingers?"
+fi
