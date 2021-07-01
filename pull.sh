39489767582c6d78e2d1d68ee879c8f512bb166c
TerraCoders Web Dev <services@terracoders.support>
2021-06-30 22:54:34 -0600

    Automate Git Pull

--- pull.sh
+++ pull.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 
+# Parse current branch from 'git branch'
 current="$(git branch | grep "*" | cut -b 3-)"
 
 echo "You are currently checked out on origin/$current" | lolcat
 
+# if current branch is master
 if [ "$current" == master ]; then
   echo "Push to origin/master (y)?"
   read -i "y" -e yn1
   [ "$yn1" == y ] && git pull origin master || echo "Fat fingers?"
 else
+  # otherwise, push to current branch
   echo "Push to origin/$current (y)?"
   read -i "y" -e yn2
   [ "$yn2" == y ] && git pull origin $current || echo "Fat fingers?"
