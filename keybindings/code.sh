6fd99953a62939256c4818c0f1e99d0cbe95b543
David <services@terracoders.support>
2021-07-05 22:27:34 -0600

    'Key Bindings'

---
+++ keybindings/code.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# Bring terminal to front (for use with key-binding)
+# See xfce4-keyboard-settings: alt+v
+
+xdotool search --desktop 0 code windowactivate
---
+++ keybindings/terminal.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# Bring terminal to front (for use with key-binding)
+# See xfce4-keyboard-settings: alt+g
+
+xdotool search --desktop 0 terminal windowactivate
