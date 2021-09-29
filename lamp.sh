91486053ebaff769378d4ee324a843d595e82c73
David <services@terracoders.support>
2021-09-29 15:37:41 -0600

    'lamp installer'

---
+++ lamp.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+ #######################################
+
+ # Bash script to install an LAMP stack in ubuntu
+
+ # Check if running as root
+ if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+ fi
+
+ # Ask value for mysql root password
+ read -p 'db_root_password [secretpasswd]: ' db_root_password
+ echo
+
+ #add PHP repository if not already added
+ ondrej="$(grep ondrej /etc/apt/sources.list /etc/apt/sources.list.d/*)"
+ [ "$ondrej" == "" ] && sudo add-apt-repository ppa:ondrej/php
+
+ # Update system
+ sudo apt-get update -y
+
+ ## Install APache
+ sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+
+ ## Install PHP
+ apt-get install php7.4 libapache2-mod-php php-mysql -y
+
+ # Install MySQL database server
+ export DEBIAN_FRONTEND="noninteractive"
+ echo "mysql-server mysql-server/root_password password ${db_root_password}" | debconf-set-selections
+ echo "mysql-server mysql-server/root_password_again password ${db_root_password}" | debconf-set-selections
+ apt-get install mysql-server -y
+
+ # Enabling Mod Rewrite
+ sudo a2enmod rewrite
+ sudo php5enmod mcrypt
+
+ ## Install PhpMyAdmin
+ sudo apt-get install phpmyadmin -y
+
+ ## Configure PhpMyAdmin
+ echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf
+
+ # Set Permissions
+ sudo chown -R www-data:www-data /var/www
+
+ # Restart Apache
+ sudo service apache2 restart
