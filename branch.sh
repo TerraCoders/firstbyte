9f3dc9204c56ef34c157a0e013a0ee465414a5eb
David <services@terracoders.support>
2021-10-06 15:46:43 -0600

    'Easier creation of git branches'

--- branch.sh
+++ branch.sh
@@ -1,22 +1,19 @@
 #! /bin/bash
-# https://terracoders.com/blog/bash-simple-script-checking-out-new-git-branches
-# An easy script for checking out new branches with git.
-# The script asks for a new branch name, identifies the current branch
-# and then asks whether you'd like to create the new branch for the (c)urrent branch
-# or from (m)aster. Abort by entering any value other than C or M at the prompt
 
 current="$(git branch | grep "*" | cut -b 3-)"
 
-echo "Name of new branch?"
+# if arg is not set
+if [ -z "${1}" ]; then
+  echo "Name of new branch?"
+  read branch
 
-read branch
-
-echo "Create $branch from $current (c) or from Master (m)?"
-read -p "current(c) / Master(m) ?" cm
-if [ $cm == 'c' ]; then
-    git checkout -b $branch
-elif [ $cm == 'm' ]; then
-    git checkout master && git pull origin master && git checkout -b $branch
+  echo "Create $branch from $current (c) or from Master (m)?"
+  read -p "current(c) / Master(m) ?" cm
+  [ $cm == 'c' ] && git checkout -b $branch
+  [ $cm == 'm' ] && git checkout master && git pull origin master && git checkout -b $branch
 else
-    echo "Fat fingers?" | lolcat
+ echo "Create $1 from $current (c) or from Master (m)?"
+  read -p "current(c) / Master(m) ?" cm
+  [ $cm == 'c' ] && git checkout -b "$1"
+  [ $cm == 'm' ] && git checkout master && git pull origin master && git checkout -b "$1"
 fi
