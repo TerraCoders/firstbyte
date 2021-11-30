506d3f472a61f4bc9a4a090d9646b844ffd02b59
David Diaz <services@terracoders.support>
2021-11-30 12:52:00 -0700

    'MineCraft Sever Management Scripts'

--- MineCraft/newworld.sh
+++ MineCraft/newworld.sh
@@ -6,8 +6,8 @@ ssh user@192.168.0.XX << 'EOF'
 num="$(echo $RANDOM)"
 # store new level name based on random number as a string
 new="level-name=$num"
-# find the line in server.properties that stores level name
+# find the line number in server.properties that defines level name
 line="$(awk 'level-name/{print NR}' /serverfiles/server.properties)"
-
+# delete existing level name at line number and replace with $new 
 sed -i "${line}d" /serverfiles/server.properties && sed -i "${line}i ${new}" /serverfiles/server.properties
 EOF
---
+++ MineCraft/readme.txt
@@ -0,0 +1,6 @@
+These Bash scripts are written for the Java (paper.jar) version of MineCraft Server (1.17+) built on
+balenaCloud for the Raspberry Pi. 
+
+I've specifically written these scripts to encourage my kids (currently 6th graders) to manage their
+MC server. I use lolcat, jp2a and cowsay to keep things colorful and fun. You can either remove these
+commands from the scripts or you can install them with apt.
