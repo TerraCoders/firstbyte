613bd5f8031f0f8e8730a6671a3f8e1f9448233b
TerraCoders Web Dev <services@terracoders.support>
2021-12-01 11:49:48 -0700

    Easy plugin installer for your MC Server

--- MineCraft/pi.sh
+++ MineCraft/pi.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
 
-# script for automating scp file transfers to Java version RPi MC Server (paper)
+# Script for automating scp file transfers to Java version RPi MC Server (paper.jar)
+# In my case, this script is set up to pass files to the server's plugins folder--since  
+# most of the files that ever need to be send to the server are plugins. You will need to know
+# your server's IP address, user and password. You will also want to confirm the path to your 
+# plugins folder (if you intend to use the script for plugins).
 
 #########
 # USAGE #
 #########
 
-# example: ./pi.sh ~/path/to/file.jar
-# example: ./pi.sh file.jar
+# example: ./pi.sh ~/path/to/plugin.jar
+# example: ./pi.sh plugin.jar
 
 # Local or remote RPi address
 # Change [user] to whatever user you log in as
 host="user@XXX.XXX.X.XX"
 
 # Path to RPi plugins folder
-# Most transfers go to the plugins folder, but you can play with path as necessary;
+# Most of my transfers go to the plugins folder, but you can play with path as necessary;
 # You might even print a list of destinations and then select one with read
 path=":/serverfiles/plugins"
 
@@ -27,7 +31,7 @@ if [ -f "$1" ]; then
   echo "Sending $1 to your MineCraft Server"
   # remove path and isolate file name
   file="$(basename $1)"
-  # use scp to send file
+  # use scp to send file (from the second command is optional--I use jp2a, cowsay and lolcat for my kids
   scp "$1" "$destination"/"$file" && jp2a ~/Scripts/src/minecraft.jpg --invert --term-fit | lolcat && cowsay "$file has been sent to $destination." | lolcat
 
 else
