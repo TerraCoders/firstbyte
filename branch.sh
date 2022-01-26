6c86eabac9da62042b46088f6ec7d1c08d6f3dc5
David Diaz <services@terracoders.support>
2022-01-26 10:52:31 -0700

    'Automated Branch Creation for Git'

--- branch.sh
+++ branch.sh
@@ -11,15 +11,26 @@ current="$(git branch | grep "*" | cut -b 3-)"
 if [ -z "${1}" ]; then
   echo "Name of new branch?"
   read branch
-
-  echo "Create $branch from $current (c) or from Master (m)?"
-  read -p "current(c) / Master(m) ?" cm
-  [ $cm == 'c' ] && git checkout -b $branch
-  [ $cm == 'm' ] && git checkout master && git pull origin master && git checkout -b $branch
+  # and if on master
+  if [ "$current" == 'master' ];then
+  echo "Creating $branch from Master..."
+  git checkout master && git pull origin master && git checkout -b "$branch"
+  else
+    #if not on master
+    echo "Create $branch from $current (c) or from Master (m)?"
+    read -p "current(c) / Master(m) ?" cm
+    [ $cm == 'c' ] && git checkout -b "$branch"
+    [ $cm == 'm' ] && git checkout master && git pull origin master && git checkout -b "$branch"
+  fi
 #otherwise use $1 arg as branch name
 else
- echo "Create $1 from $current (c) or from Master (m)?"
-  read -p "current(c) / Master(m) ?" cm
-  [ $cm == 'c' ] && git checkout -b "$1"
-  [ $cm == 'm' ] && git checkout master && git pull origin master && git checkout -b "$1"
+  if [ "$current" == 'master' ];then
+  echo "Creating $1 from Master..."
+  git checkout master && git pull origin master && git checkout -b "$1"
+  else
+    echo "Create $1 from $current (c) or from Master (m)?"
+    read -p "current(c) / Master(m) ?" cm
+    [ $cm == 'c' ] && git checkout -b "$1"
+    [ $cm == 'm' ] && git checkout master && git pull origin master && git checkout -b "$1"
+  fi
 fi
